2c003ec6b88eb2e22dfaf96ea58f0c68f1e8269d
Hussein Mahfouz <h.mahfouz@eweev.com>
2024-02-05 10:02:43 +0200

    Component for medias regarding its type #30

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.8] - 2024-02-05
+
+### Added
+
+- A view component for medias regarding its type #30
+
 ## [1.0.7] - 2024-01-31
 
 ### Added
---
+++ lib/src/components/SectionsViews/MediaViewer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="media && media.url" class="media-viewer">
+    <div v-if="media.type === 'image'" class="media-viewer-image-wrapper">
+      <img :src="media.url" :alt="media.seo_tag ? media.seo_tag : ''" class="media-viewer-image">
+    </div>
+    <div v-else-if="media.type === 'document'" class="media-viewer-document-wrapper">
+      <iframe :src="media.url" :title="media.seo_tag ? media.seo_tag : ''" class="media-viewer-document"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MediaViewer",
+  props: {
+    media: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
