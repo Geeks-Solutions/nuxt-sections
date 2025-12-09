467f0b22d08a1fe9ea41b50ed53389770dfd17af
Julien <julien@geeks.solutions>
2025-12-09 20:13:51 +0100

    fix: move mediaTransformer DI into global-hooks file

--- src/runtime/plugins/media-transformer.ts
+++ src/runtime/plugins/media-transformer.ts
@@ -1,13 +1,14 @@
 import { defineNuxtPlugin } from '#app'
+import { importJs } from '../utils/helpers'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     try {
-        // Import media_transform from the app's media-transform module
-        const mediaTransformModule = await import('/sections/js/media-transform.js')
-        if (mediaTransformModule && mediaTransformModule.media_transform) {
-            nuxtApp.vueApp.provide(Symbol.for('mediaURLTransformer'), mediaTransformModule.media_transform)
+        // Import media_transform from the app's global-hooks module
+        const globalHooks = importJs('/js/global-hooks')
+        if (globalHooks && globalHooks.media_transform) {
+            nuxtApp.vueApp.provide(Symbol.for('mediaURLTransformer'), globalHooks.media_transform)
         }
     } catch (e) {
-        // Ignore error if media-transform cannot be loaded
+        // Ignore error if global-hooks cannot be loaded 
     }
 })
