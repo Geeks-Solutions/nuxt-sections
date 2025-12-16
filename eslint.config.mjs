4697fe8dac27d5ec04fb01ab1098ca1323b81bce
Julien <julien@geeks.solutions>
2025-12-16 19:18:19 +0100

    synced eslint config automatic formatting in vscode vscode extensions settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
@@ -12,7 +14,20 @@ export default createConfigForNuxt({
   dirs: {
     src: ['./playground'],
   },
-})
-  .append
-  // your custom flat config here...
-  ()
+}).append(
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-useless-escape': 'off',
+      'vue/no-v-html': 'off',
+      'vue/no-use-v-if-with-v-for': 'off',
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-undef': 'off', // Temporary disable to clear noise if globals are used
+    },
+  },
+  eslintConfigPrettier
+)
